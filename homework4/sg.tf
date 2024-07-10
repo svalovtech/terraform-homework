b6b344ddf507acc9ea8cb4c491203f1c071d3734
svalovtech <svalovtech@gmail.com>
2024-07-10 21:33:57 +0000

    change sg

--- homework4/sg.tf
+++ homework4/sg.tf
@@ -2,39 +2,39 @@ resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
 
-   dynamic "ingress" {
-    for_each = var.allow_ports
-    content {
-    from_port        = ingress.value
-    to_port          = ingress.value
+  #  dynamic "ingress" {
+  #   for_each = var.allow_ports
+  #   content {
+  #   from_port        = ingress.value
+  #   to_port          = ingress.value
+  #   protocol         = "tcp"
+  #   cidr_blocks      = ["0.0.0.0/0"]
+  # }
+  #  }
+
+    ingress {
+    description      = "TLS from VPC"
+    from_port        = var.allow_ports[0]
+    to_port          = var.allow_ports[0]
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-   }
 
-#     ingress {
-#     description      = "TLS from VPC"
-#     from_port        = 80
-#     to_port          = 80
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#   }
+    ingress {
+    description      = "TLS from VPC"
+    from_port        = var.allow_ports[1]
+    to_port          = var.allow_ports[1]
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
-#     ingress {
-#     description      = "TLS from VPC"
-#     from_port        = 443
-#     to_port          = 443
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#   }
-
-#    ingress {
-#     description      = "TLS from VPC"
-#     from_port        = 22
-#     to_port          = 22
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#   }
+   ingress {
+    description      = "TLS from VPC"
+    from_port        = var.allow_ports[2]
+    to_port          = var.allow_ports[2]
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
    egress {
     from_port        = 0
